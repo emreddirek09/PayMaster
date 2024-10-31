f9d19903df223de644439e572413c3a6c576aafd
Emre <emredirek1905@gmail.com>
2024-10-31 13:11:44 +0300

    Validation - DTO

---
+++ PayMasterProject.BusinessLayer/ValidationRules/UserValidationRules/UserRegisterValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using PayMasterProject.DtoLayer.Dtos.UserRegisterDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayMasterProject.BusinessLayer.ValidationRules.UserValidationRules
+{
+    public class UserRegisterValidator : AbstractValidator<UserRegisterDto>
+    {
+        public UserRegisterValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Ad alanı boş geçilemez");  
+            RuleFor(x => x.SurName).NotEmpty().WithMessage("Soyad alanı boş geçilemez");    
+            RuleFor(x => x.Email).NotEmpty().WithMessage("Eposta alanı boş geçilemez");
+            RuleFor(x => x.Email).EmailAddress().WithMessage("Lütfen geçerli eposta giriniz.");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Şifre alanı boş geçilemez");
+            RuleFor(x => x.ConfirmPassword).NotEmpty().WithMessage("Tekrar şifre alanı boş geçilemez");    
+            RuleFor(x => x.ConfirmPassword).Equal(y=>y.Password).WithMessage("Şifre eşleşmiyor.");    
+            
+        }
+    }
+}
---
+++ PayMasterProject.DtoLayer/Dtos/UserRegisterDtos/UserRegisterDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayMasterProject.DtoLayer.Dtos.UserRegisterDtos
+{
+    public class UserRegisterDto
+    {
+        [Required(ErrorMessage ="Ad Alanı Zorunludur")]
+        [Display(Name="İsim")]
+        [MaxLength(30,ErrorMessage ="En fazla 30 karakter girebilirsiniz")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Ad Alanı Zorunludur")]
+        [Display(Name = "Soyisim")]
+        [MaxLength(15, ErrorMessage = "En fazla 15 karakter girebilirsiniz")]
+        public string SurName { get; set; }
+
+        [Required(ErrorMessage = "Email Alanı Zorunludur")]
+        [Display(Name = "E-Posta")]
+        [MaxLength(100, ErrorMessage = "En fazla 100 karakter girebilirsiniz")]
+        public string Email { get; set; } 
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
